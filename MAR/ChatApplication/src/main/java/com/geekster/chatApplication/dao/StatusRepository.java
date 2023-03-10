506c28558b82468c8c7985aa2cef949141acb0dd
Hrishikesh Kaleking <hrishikeshkalekinge31@gmail.com>
2023-03-10 12:21:13 +0530

    .

--- MAR/ChatApplication/src/main/java/com/geekster/chatApplication/dao/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.geekster.chatApplication.dao;
-
-import com.geekster.chatApplication.model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
