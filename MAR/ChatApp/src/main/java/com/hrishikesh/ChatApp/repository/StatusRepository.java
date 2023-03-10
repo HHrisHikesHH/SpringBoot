cb09dcfe6a78bf3dd707bab1415f618b5729dba4
Hrishikesh Kaleking <hrishikeshkalekinge31@gmail.com>
2023-03-10 12:19:15 +0530

    .

--- MAR/ChatApp/src/main/java/com/hrishikesh/ChatApp/repository/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hrishikesh.ChatApp.repository;
-
-import com.hrishikesh.ChatApp.model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-}
--- MAR/ChatApp/src/main/java/com/hrishikesh/ChatApp/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hrishikesh.ChatApp.repository;
-
-import com.hrishikesh.ChatApp.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-}
