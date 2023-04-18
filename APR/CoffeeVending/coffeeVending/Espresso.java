f2402a2a056d4954263838e4bc17d4fb042366a3
hrishikesh kalekinge <100271790+HHrisHikesHH@users.noreply.github.com>
2023-04-18 11:13:45 +0530

    Update Espresso.java

--- APR/CoffeeVending/coffeeVending/Espresso.java
+++ APR/CoffeeVending/coffeeVending/Espresso.java
@@ -1,5 +1,10 @@
 package coffeeVending;
 
 public class Espresso {
-
+      public String orderReady() {
+                    Coffe.coffee_beans = Coffe.coffee_beans - 16;
+                    Coffe.water -= 250;
+                    Coffe.wallet += 4;
+                    return "Your Order Are Ready !";
+          }
 }
