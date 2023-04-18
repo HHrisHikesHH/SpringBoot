b363684eb6be06c4047edfc24cecb6e03db04db2
hrishikesh kalekinge <100271790+HHrisHikesHH@users.noreply.github.com>
2023-04-18 11:12:42 +0530

    Update Cappuccino.java

--- APR/CoffeeVending/coffeeVending/Cappuccino.java
+++ APR/CoffeeVending/coffeeVending/Cappuccino.java
@@ -1,5 +1,11 @@
 package coffeeVending;
 
 public class Cappuccino {
-
+    public String orderReady() {
+                    Coffe.coffee_beans -= 12;
+                    Coffe.water -= 200;
+                    Coffe.milk -= 100;
+                    Coffe.wallet += 6;
+                    return "Your Order Are Ready !";
+          }
 }
