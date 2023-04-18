86de7addef2acba87398c0bc7260f770b3f874c6
hrishikesh kalekinge <100271790+HHrisHikesHH@users.noreply.github.com>
2023-04-18 11:14:02 +0530

    Update Latte.java

--- APR/CoffeeVending/coffeeVending/Latte.java
+++ APR/CoffeeVending/coffeeVending/Latte.java
@@ -1,5 +1,11 @@
 package coffeeVending;
 
 public class Latte {
-
+    public String orderReady() {
+                    Coffe.coffee_beans -= 20;
+                    Coffe.water -= 350;
+                    Coffe.milk -= 75;
+                    Coffe.wallet += 7;
+                    return "Your Order Are Ready !";
+          }
 }
