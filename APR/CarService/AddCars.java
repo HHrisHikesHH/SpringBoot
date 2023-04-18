c1ec1d1a4e6af884e179e3ec778f856792ec0000
hrishikesh kalekinge <100271790+HHrisHikesHH@users.noreply.github.com>
2023-04-18 11:27:21 +0530

    Update AddCars.java

--- APR/CarService/AddCars.java
+++ APR/CarService/AddCars.java
@@ -1,1 +1,21 @@
 
+import java.util.HashMap;
+
+public class AddCars {
+          private String cartype;
+          private String serviceCode;
+          private int charge;
+
+          public AddCars(String cartype, String serviceCode, int charge) {
+                    this.cartype = cartype;
+                    this.serviceCode = serviceCode;
+                    this.charge = charge;
+                    HashMap<String, Integer> service = new HashMap<>();
+                    service.put(serviceCode, charge);
+                    car.cars.put(cartype, service);
+          }
+
+          public void getResult() {
+                    System.out.println("Okk Checked");
+          }
+}
